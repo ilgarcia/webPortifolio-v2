38effc32a69aca5aa4a534d8c25e709368e0ae98
ilgarcia <60369262+ilgarcia@users.noreply.github.com>
2023-12-11 22:56:40 -0300

    changing skills section style

--- src/components/skills/Badges.tsx
+++ src/components/skills/Badges.tsx
@@ -7,7 +7,7 @@ type Props = {
 function Badges({ skill }: Props) {
 
   return (
-    <div className="flex flex-col items-center gap-1 sm:gap-1.5 md:gap-2.5 h-fit w-[4.5rem] sm:w-20 lg:w-28 py-3 lg:py-5 text-orange-400 bg-slate-900 text-xs lg:text-sm rounded-ss-2xl border-b-4 border-orange-400 border-solid ease-in-out transition-transform duration-300 hover:scale-125 hover:z-10">
+    <div className="flex flex-col items-center gap-1.5 md:gap-2.5 h-fit w-20 lg:w-28 py-3 lg:py-5 text-orange-400 bg-slate-900 text-sm rounded-ss-2xl border-b-4 border-orange-400 border-solid ease-in-out transition-transform duration-300 hover:scale-125 hover:z-10">
       <p>{skill.title}</p>
       {DynamicIconSi(skill.icon)}
     </div>
