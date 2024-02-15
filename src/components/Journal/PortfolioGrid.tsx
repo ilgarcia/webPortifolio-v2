9cff9b1a9c8064a906e4af23815fb77f8b67f677
Ilgarcia <limagarcia.igor@gmail.com>
2024-02-15 20:02:03 -0300

    fixed error of multiples requests

--- src/components/Journal/PortfolioGrid.tsx
+++ src/components/Journal/PortfolioGrid.tsx
@@ -1,33 +1,19 @@
 "use client";
 
-import { useCycle } from "framer-motion";
 import PortfolioCard from "./PortfolioCard";
-import HoverEffect from "./HoverEffect";
 
 type Props = {
   portfolio: Portfolio[];
 };
 
 function PortfolioGrid({ portfolio }: Props) {
-  const [showMore, toggleShowMore] = useCycle(false, true);
-
   return (
     <section id="moreProjects">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-5xl mx-auto">
-        {portfolio.slice(0, showMore ? 11 : 6).map((project, id) => (
+        {portfolio.map((project, id) => (
           <PortfolioCard key={id} project={project} id={id} />
         ))}
-        {portfolio.length > 11 && showMore && (
-          <HoverEffect id={12}>Teste</HoverEffect>
-        )}
       </div>
-      {portfolio.length > 6 && (
-        <div>
-          <button onClick={() => toggleShowMore()}>
-            {showMore ? "Show less" : "Show more"}
-          </button>
-        </div>
-      )}
     </section>
   );
 }
--- src/components/Journal/Tabs.tsx
+++ src/components/Journal/Tabs.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useCallback, useEffect, useState } from "react";
-import { useSearchParams, } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import Image from "next/image";
 import { groq } from "next-sanity";
 
@@ -12,58 +12,45 @@ import { client } from "../../../sanity/lib/client";
 
 import * as Tb from "@radix-ui/react-tabs";
 
-// type Props = {
-//   portfolio: Portfolio[];
-//   posts: Post[];
-// };
+// const queryPortfolio = groq`
+//   *[_type=='portfolio']{
+//     ...,
+//     appType->,
+//     skill[]->,
+//     post[]->,
+//   } | order(_createdAt desc)
+// `;
 
-const queryPortfolio = groq`
-  *[_type=='portfolio']{
-    ...,
-    appType->,
-    skill[]->,
-    post[]->,
-  } | order(_createdAt desc)
-`;
+// const queryPosts = groq`
+//   *[_type=='post']{
+//     ...,
+//     category->,
+//     skill[]->,
+//     post[]->,
+//     portfolio[]->,
+//   } | order(_createdAt desc)
+// `;
 
-const queryPosts = groq`
-  *[_type=='post']{
-    ...,
-    category->,
-    skill[]->,
-    post[]->,
-    portfolio[]->,
-  } | order(_createdAt desc)
-`;
+type Props = {
+  portfolio: Portfolio[];
+  posts: Post[];
+};
 
-function Tabs() {
-
-  const searchParams = useSearchParams(); 
+function Tabs({ portfolio, posts }: Props) {
+  const searchParams = useSearchParams();
   const tabName = searchParams.get("tab") || "posts";
 
-  const [portfolio, setPortfolio] = useState([]);
-  const [posts, setPosts] = useState([]);
+  // const [portfolio, setPortfolio] = useState([]);
+  // const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      await client.fetch(queryPortfolio).then((data) => setPortfolio(data));
-      await client.fetch(queryPosts).then((data) => setPosts(data));
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     await client.fetch(queryPortfolio).then((data) => setPortfolio(data));
+  //     await client.fetch(queryPosts).then((data) => setPosts(data));
+  //   };
 
-    fetchData();
-  });
-
-  // // Get a new searchParams string by merging the current
-  // // searchParams with a provided key/value pair
-  // const createQueryString = useCallback(
-  //   (name: string, value: string) => {
-  //     const params = new URLSearchParams(searchParams);
-  //     params.set(name, value);
-
-  //     return params.toString();
-  //   },
-  //   [searchParams]
-  // );
+  //   fetchData();
+  // });
 
   return (
     <Tb.Root
@@ -134,116 +121,6 @@ function Tabs() {
       </Tb.Content>
     </Tb.Root>
   );
-
-  // return (
-  //   <div className="relative max-w-7xl min-h-[80vh] mx-auto -mt-4 z-20">
-  //     <div className="relative mt-10 flex justify-between items-center max-w-4xl mx-auto text-2xl font-medium font-fira ">
-  //       <Image
-  //         src="/divLine.svg"
-  //         alt="divLine"
-  //         width={25}
-  //         height={85}
-  //         className="relative left-1/2 top-1/2 w-10"
-  //       />
-  //       <div className="relative w-full py-4 text-center">
-  // <Image
-  //   src="/blogHyperline.svg"
-  //   alt="divLine"
-  //   width={160}
-  //   height={38}
-  //   className={`absolute left-1/2  -translate-x-1/2 -translate-y-1/4 transition-all duration-500 ${
-  //     tabName === "posts" ? "opacity-50 scale-110" : "opacity-0 scale-0"
-  //   }`}
-  // />
-  //         <button
-  // onClick={() =>
-  //   router.push(pathname + "?" + createQueryString("tab", "posts"))
-  // }
-  //           className={`group relative bottom-0 transition-all duration-300  ${
-  //             tabName === "posts"
-  //               ? "text-slate-200 hover:animate-wiggle"
-  //               : "text-slate-400 hover:text-slate-200"
-  //           }`}
-  //         >
-  // <span
-  //   className={`absolute -left-6 transition-all duration-300 delay-200 text-slate-400 ${
-  //     tabName === "posts"
-  //       ? "translate-x-2"
-  //       : "translate-x-0 group-hover:translate-x-2"
-  //   }`}
-  // >
-  //   &#60;
-  // </span>
-  // <span
-  //   className={`text-xl absolute -left-3 transition-all duration-300 text-slate-400 ${
-  //     tabName === "posts"
-  //       ? "opacity-0"
-  //       : "opacity-100 group-hover:opacity-0"
-  //   }`}
-  // >
-  //   /
-  // </span>
-  // Blog
-  // <span className="text-slate-400 ">&#62;</span>
-  //         </button>
-  //       </div>
-  //       <div className="relative w-full py-4 text-center">
-  //         <Image
-  //           src="/blogHyperline.svg"
-  //           alt="divLine"
-  //           width={160}
-  //           height={38}
-  //           className={`absolute left-1/2  -translate-x-1/2 -translate-y-1/4 transition-all duration-500 ${
-  //             tabName === "portfolio"
-  //               ? "opacity-50 scale-110"
-  //               : "opacity-0 scale-0"
-  //           }`}
-  //         />
-  //         <button
-  //           onClick={() =>
-  //             router.push(
-  //               pathname + "?" + createQueryString("tab", "portfolio")
-  //             )
-  //           }
-  //           className={`group relative bottom-0 transition-all duration-300  ${
-  //             tabName === "portfolio"
-  //               ? "text-slate-200 hover:animate-wiggle"
-  //               : "text-slate-400 hover:text-slate-200"
-  //           }`}
-  //         >
-  //           <span
-  //             className={`absolute -left-6 transition-all duration-300 delay-200 text-slate-400 ${
-  //               tabName === "portfolio"
-  //                 ? "translate-x-2"
-  //                 : "translate-x-0 group-hover:translate-x-2"
-  //             }`}
-  //           >
-  //             &#60;
-  //           </span>
-  //           <span
-  //             className={`text-xl absolute -left-3 transition-all duration-300 text-slate-400 ${
-  //               tabName === "portfolio"
-  //                 ? "opacity-0"
-  //                 : "opacity-100 group-hover:opacity-0"
-  //             }`}
-  //           >
-  //             /
-  //           </span>
-  //           Portfolio
-  //           <span className="text-slate-400 ">&#62;</span>
-  //         </button>
-  //       </div>
-  //     </div>
-
-  //     <div>
-  //       {tabName === "posts" ? (
-  //         <Posts posts={posts} />
-  //       ) : (
-  //         <PortfolioGrid portfolio={portfolio} />
-  //       )}
-  //     </div>
-  //   </div>
-  // );
 }
 
 export default Tabs;
