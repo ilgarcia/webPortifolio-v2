0d788ef0cb1c66573a6a91bbc8aa9629f1db38f5
Ilgarcia <limagarcia.igor@gmail.com>
2024-05-13 17:55:16 -0300

    modules update

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,19 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+
+import blogContent from "./schemas/blockBlogContent";
+import workContent from "./schemas/blockWorkContent";
+import workExperience from "./schemas/workExperience";
+import appType from "./schemas/appType";
+import skill from "./schemas/skill";
+import portfolio from "./schemas/portfolio";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [
+    skill,
+    portfolio,
+    appType,
+    workExperience,
+    workContent,
+    blogContent,
+  ],
+};
